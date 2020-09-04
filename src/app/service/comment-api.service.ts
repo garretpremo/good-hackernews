2e333a99d5053e28d2d2685490acaa8a9ed7ea89
gpremo <garretpremo@gmail.com>
2020-09-04 19:05:01 -0400

    Fixed comments loading infinitely

--- src/app/service/comment-api.service.ts
+++ src/app/service/comment-api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { combineLatest, Observable, of } from 'rxjs';
+import { forkJoin, Observable } from 'rxjs';
 import { StoryComment } from '../shared/models/story-comment.model';
 import { map, tap } from 'rxjs/operators';
 
@@ -13,7 +13,7 @@ export class CommentApiService {
   }
 
   getComments(ids: number[]): Observable<StoryComment[]> {
-    return combineLatest(ids.map(id => this.getById(id)));
+    return forkJoin(ids.map(id => this.getById(id)));
   }
 
   getById(id: number): Observable<StoryComment> {
@@ -21,7 +21,7 @@ export class CommentApiService {
       .pipe(
         map(comment => StoryComment.fromApi(comment)),
         tap(comment => {
-          if (comment?.kids) {
+          if (comment?.kids?.length > 0) {
             this.getComments(comment.kids).subscribe(subComments => {
               subComments.forEach(subComment => comment.subCommentCount += subComment.by ? (subComment.subCommentCount + 1) : 0);
               comment.subComments = subComments;
@@ -29,8 +29,6 @@ export class CommentApiService {
           } else {
             comment.subComments = [];
           }
-
-          return of(comment);
         })
       );
   }
